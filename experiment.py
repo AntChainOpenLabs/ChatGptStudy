505128da26bbb5599405df519e062ba1517ccebe
spacexchen <tkdjsoo@outlook.com>
2023-05-30 14:00:06 +0800

    test FineTune 1G

--- experiment.py
+++ experiment.py
@@ -3,9 +3,11 @@ from staVul2proComp import *
 
 # 创建一个空列表来存储所有字典
 vulnerabilities = []
+benign = []
 # prompt&completion文件
 wirtefilename = r"experiment\1\prompt_completion_1G.jsonl"
-
+wirtefilename2 = r"experiment\1\prompt_completion_1G_benign.jsonl"
+testfilename = r"experiment\1\prompt_completion_1G_test.jsonl"
 # prompt优化字符
 prompt_end = "\n\n###\n\n"
 completion_start = " "
@@ -33,12 +35,32 @@ def read_file(filename,num_bug):
         if i > num_bug:
             break
 
+def read_benign_file(filename,num_bug):
+    # 打开JSON文件并加载数据
+    with open(filename, encoding='utf-8') as f:
+        data = json.load(f)
 
-def generate_prompt_completion(filename):
+    # 遍历JSON中的每个对象
+    i = 0
+    for item in data:
+        # 创建一个字典来存储此对象的信息
+        benign_example = {}
+        benign_example['Code'] = item['prompt']
+        benign_example["IsBug"] = item["completion"]
+
+        # 将此漏洞字典添加到漏洞列表中
+        benign.append(benign_example)
+        i+=1
+    if i<num_bug:
+        print("benign example not enough")
+
+def generate_prompt_completion(filename, dic, choice):
     with open(filename,"w") as f :
-        for vul in vulnerabilities:
-            prompt_completion_fake_deposit(f, vul)
-
+        for vul in dic:
+            if choice == 1:
+                prompt_completion_fake_deposit(f, vul)
+            elif choice == 2:
+                prompt_completion_benign(f,vul)
 
 
 def prompt_completion_fake_deposit(f, vul):
@@ -49,13 +71,21 @@ def prompt_completion_fake_deposit(f, vul):
     write_file(prompt_completion,f)
 
 
+def prompt_completion_benign(f, vul):
+    prompt = "Does this  smart contract  have  fake deposit vulnerability?  " + "\n" + vul['Code']
+
+    prompt_completion = {"prompt": prompt}
+    write_file(prompt_completion, f)
 
 if __name__ == "__main__":
     good = 300
     bad = 300
     read_file(r"experiment\1\fake_deposit_final.json",bad)
-    read_file(r"experiment\1\Benign_sample_Check_token_True.json",good)
-    generate_prompt_completion(wirtefilename)
+    read_benign_file(r"experiment\1\Benign_sample_Check_token_True.json",good)
+    generate_prompt_completion(wirtefilename,vulnerabilities,1)
+    generate_prompt_completion(wirtefilename2,benign[:good],1)
+    generate_prompt_completion(testfilename,benign[good+1:],2)
+
 
 
 
---
+++ testFineTune.py
@@ -0,0 +1,50 @@
+import openai
+import json
+prompt_end = "\n\n###\n\n"
+completion_start = " "
+completion_end = "###"
+FINE_TUNED_MODEL = "davinci:ft-antchainopenlab-2023-05-29-14-14-47"
+testfilename = r"experiment\1\prompt_completion_1G_test.jsonl"
+testResult = r"experiment\1\prompt_completion_1G_result2.jsonl"
+
+
+def read_testFile(file, testCaseNum):
+    i = 1
+    testCase = []
+    with open(file,"r") as f:
+        for line in f.readlines():
+            if i > testCaseNum : #只取适量的测试样本
+                break
+            line = json.loads(line)
+            testCase.append(line["prompt"])
+            i+=1
+    return testCase
+
+
+
+
+def test_fineTune(FINE_TUNED_MODEL , testCase , file):
+    ans_js =  {}
+    for i in range(len(testCase)):
+        PROMPT = testCase[i]
+        ans = []
+        print("正在测试第{}个样本\n".format(i))
+        for j in range(10):
+            response = openai.Completion.create(
+                model= FINE_TUNED_MODEL,
+                prompt= PROMPT + prompt_end,
+                stop=completion_end,
+                temperature= 2,
+                n = 10
+            )
+            for choice in response["choices"]:
+                ans.append(choice["text"].replace("\n",""))
+        ans_js[i] = ans
+
+    with open(file,"w") as f:
+        json.dump(ans_js,f,indent=4)
+
+
+if __name__ == "__main__":
+    testCase = read_testFile( testfilename , 10)
+    test_fineTune(FINE_TUNED_MODEL, testCase , testResult)
