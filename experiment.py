3d03c7b581ff9810e8cb103ff4c99fa924241949
spacexchen <tkdjsoo@outlook.com>
2023-06-01 22:13:17 +0800

    change data for train and vaild

--- experiment.py
+++ experiment.py
@@ -5,10 +5,11 @@ from staVul2proComp import *
 vulnerabilities = []
 benign = []
 # prompt&completion文件
-wirtefilename = r"experiment\1\1A\prompt_completion_1A.jsonl"
-wirtefilename2 = r"experiment\1\1A\prompt_completion_1A_benign.jsonl"
-testfilename = r"experiment\1\1A\prompt_completion_1A_test.jsonl"
-testfilename_bug = r"experiment\1\1A\prompt_completion_1A_test_bug.jsonl"
+wirtefilename = r"experiment\1\1G\prompt_completion_1G.jsonl"
+wirtefilename2 = r"experiment\1\prompt_completion_train_benign.jsonl"
+
+testfilename = r"experiment\1\1G\prompt_completion_test_benign.jsonl"
+testfilename_bug = r"experiment\1\1G\prompt_completion_test_bug.jsonl"
 # prompt优化字符
 prompt_end = "\n\n###\n\n"
 completion_start = " "
@@ -20,11 +21,12 @@ completion_end = "###"
 def read_file(filename,num_bug):
     # 打开JSON文件并加载数据
     with open(filename, encoding='utf-8') as f:
-        data = json.load(f)
+        data = f.readlines()
 
     # 遍历JSON中的每个对象
     i = 0
     for item in data:
+        data = json.load(f)
         # 创建一个字典来存储此对象的信息
         vulnerability = {}
         vulnerability['Code'] = item['prompt']
@@ -56,23 +58,23 @@ def read_benign_file(filename,num_bug):
         print("benign example not enough")
 
 def generate_prompt_completion(filename, dic, choice):
-    with open(filename,"w") as f :
+    with open(filename,"w") as f:
         for vul in dic:
             if choice == 1:
                 prompt_completion_fake_deposit(f, vul)
             elif choice == 2:
-                prompt_completion_benign(f,vul)
+                prompt_completion_test(f,vul)
 
 
 def prompt_completion_fake_deposit(f, vul):
-    prompt = "Does this  smart contract  have  fake deposit vulnerability?  "  + "\n" + vul['Code']
+    prompt = "Does this  smart contract  have  fake deposit vulnerability?  " + "\n" + vul['Code']
     completion = vul['IsBug'] + "\n"
 
-    prompt_completion = prompt_optimization(prompt, completion)
+    prompt_completion = prompt_optimization(prompt,completion)
     write_file(prompt_completion,f)
 
 
-def prompt_completion_benign(f, vul):
+def prompt_completion_test(f, vul):
     prompt = "Does this  smart contract  have  fake deposit vulnerability?  " + "\n" + vul['Code']
 
     prompt_completion = {"prompt": prompt}
@@ -81,15 +83,12 @@ def prompt_completion_benign(f, vul):
 if __name__ == "__main__":
     good = 300
     bad = 300
-    read_file(r"experiment\1\fake_deposit_final.json",bad)
-    read_benign_file(r"experiment\1\Benign_sample_Check_token_True.json",good)
-    generate_prompt_completion(wirtefilename,vulnerabilities,1)
-    # generate_prompt_completion(wirtefilename2,benign[:good],1)
-    # generate_prompt_completion(testfilename,benign[good+1:],2)
+    # read_file(r"experiment\1\prompt_completion_train_bug.jsonl",bad)
+    read_benign_file(r"experiment\1\row data\Benign_sample_Check_token_True.json",good)
+
+    generate_prompt_completion(wirtefilename2,vulnerabilities+benign ,1)
 
 
-    # read_file(r"experiment\1\fake_deposit_test.json",10)
-    # generate_prompt_completion(testfilename_bug,vulnerabilities,1)
 
 
 
